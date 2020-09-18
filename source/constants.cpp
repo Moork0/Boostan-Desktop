b322e12e2214a82d83165e1a926a39b51746fd98
SeedPuller <SeedPuller@gmail.com>
2020-09-19 00:20:14 +0430

    added new error descriptions

--- source/constants.cpp
+++ source/constants.cpp
@@ -8,22 +8,29 @@ const QString Constants::application_name{"Boostan"},
               Constants::domain_name{"SeedPuller.space"},
               Constants::application_path {QDir::currentPath()};
 
+QSettings Constants::settings(QSettings::IniFormat, QSettings::UserScope, organization_name, application_name);
 
 const QHash<uint, QString> Constants::Errors::error_strings
 {
-    {Constants::Errors::ServerConnenctionError, "خطا در اتصال به سرور"}
+    {ServerConnenctionError, "خطا در اتصال به سرور"},
+    {UnknownError, "اوه! یک خطای ناشناخته رخ داده!"},
+    {CustomCode, "کد امنیتی اشتباه وارد شده"},
+    {CustomCode + 1, "مشکلی در دریافت تصویر امنیتی بوجود اومده"},
+    {1, "نام کاربری یا رمز عبور وارد شده اشتباهه"}
 };
 
 const QHash<uint, QString> Constants::Errors::error_solutions
 {
-    {Constants::Errors::ServerConnenctionError, "لطفا وضعیت اتصال به اینترنت و وبسایت گلستان رو بررسی کنید"}
+    {ServerConnenctionError, "لطفا وضعیت اتصال به اینترنت و وبسایت گلستان رو بررسی کن"},
+    {UnknownError, "از اونجایی که منم نمیدونم چه خطاییه، بهتره لاگ هارو بررسی کنی و در صفحه گیتهاب این مشکل رو گزارش بدی"},
+    {CustomCode, "دوباره با دقت تلاش کن :)"},
+    {CustomCode + 1, "دسترسی های پوشه بوستان رو بررسی کنید و برنامه رو دوباره اجرا کنید"},
+    {1, "دوباره با دقت تلاش کن :)"}
 };
 
 /*
  *  Function implementations
  */
-QSettings Constants::settings(QSettings::IniFormat, QSettings::UserScope, organization_name, application_name);
-
 QVariant Constants::getConstant(const QString &key)
 {
     return settings.value(key);
