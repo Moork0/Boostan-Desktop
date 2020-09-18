e61047971226f0e5e248190aa7c183630c1f37fe
SeedPuller <SeedPuller@gmail.com>
2020-09-19 00:21:20 +0430

    added Errors constants and moved validators to Validators namespace

--- header/textparser.h
+++ header/textparser.h
@@ -5,21 +5,35 @@
 #include <QRegularExpression>
 #include <QXmlStreamReader>
 #include <QDebug>
+#include "constants.h"
 
 using QHashString = QHash <QString, QString>;
 
 namespace TextParser
 {
 
+namespace Validators
+{
     const QString viewstate_pattern         {"__VIEWSTATE\" value=\"\\S+"};                     // regex patern for view state
     const QString viewstate_gen_pattern     {"__VIEWSTATEGENERATOR\" value=\"[A-Za-z0-9]+"};   // regex pattern for viewstate generator
     const QString event_val_pattern         {"__EVENTVALIDATION\" value=\"\\S+"};             // regex pattern for event validation
     const QString tokens_pattern            {"SavAut\\([,a-z0-9'A-Z-]+"};                    // regex pattern for lt,ctck,u,ft,seq and ...
     const QString tck_pattern               {"SetOpenerTck\\('[A-Z0-9-]{16}"};              // regex pattern for tck.
+};
+
+namespace Errors
+{
+    const QString error_withcode_pattern      {"ErrorArr = new Array\\('[\\w :]+"};
+
+    const QHash<int, QString> error_keywords {{Constants::Errors::CustomCode, "کد امنيتي"}};
+
+    int extractErrorCode(const QString& response);
+};
+
     const QString student_name_pattern      {"SetUsr\\('[\\w ,']+"};                       // regex pattern for student name
     const QString offered_course_pattern    {"<Root>[\\W\\w]+<\\/Root>"};
 
-    bool hasError(const QString& response);
+    int hasError(const QString& response);
     QHashString extractLoginValidators(const QString& response);
     QHashString extractTokens(const QString& response);
     QString extractStudentName(const QString& response);
