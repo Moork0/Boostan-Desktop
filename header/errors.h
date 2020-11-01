305c2e726b74ca48e5f127b2980655761709b617
SeedPuller <SeedPuller@gmail.com>
2020-11-01 16:33:09 +0330

    moved constants to constants.h

--- header/errors.h
+++ header/errors.h
@@ -14,30 +14,18 @@ private:
     int     error_code;
 
 public:
-    enum status{
-        Critical = 0,
-        SemiCritical,
-        Normal
+
+    enum error_type {
+        Critical        = Constants::Errors::Critical,
+        SemiCritical    = Constants::Errors::SemiCritical,
+        Normal          = Constants::Errors::Normal
     };
-    Q_ENUM(status)
+    Q_ENUM(error_type)
 
     explicit            Errors(QObject *parent = nullptr);
     void                setErrorCode(int value);
     int                 getErrorCode()         const;
     uint                getCriticalStatus()    const;
-
-private:
-        inline static const QHash<int, int> critical_status
-        {
-            {Constants::Errors::ServerConnenctionError, status::SemiCritical},
-            {Constants::Errors::UnknownError, status::Critical},
-            {Constants::Errors::WrongCaptcha, status::Normal},
-            {Constants::Errors::CaptchaStoreError, status::Critical},
-            {Constants::Errors::SettingsError, status::Critical},
-            // this is Golestan error codes that we might see.
-            {1, status::Normal},
-            {27, status::SemiCritical}
-        };
 
 public slots:
     void                 reset();
