01ac05cc4b1eea96ec81e457c8a7ab3a694eaa37
SeedPuller <SeedPuller@gmail.com>
2020-11-01 16:33:59 +0330

    dashboard handler functionalities added

--- header/dashboardhandler.h
+++ header/dashboardhandler.h
@@ -1,13 +1,41 @@
 #ifndef DASHBOARDHANDLER_H
 #define DASHBOARDHANDLER_H
 
-#include <handler.h>
+#include "handler.h"
 
 class DashboardHandler : public Handler
 {
     Q_OBJECT
+private:
+    inline static QString user_info_url {"/Forms/F1802_PROCESS_MNG_STDJAMEHMON/F1802_01_PROCESS_MNG_STDJAMEHMON_Dat.aspx?r=0.9638806400489983&fid=0;12310&b=0&l=0&&lastm=20180201081222&tck="};
+    QStringList student_info;
+    QList<QPair<int, float>> student_avgs;
+
+    bool getTokens();
+    bool getUserNumber();
+    bool getUserInfo();
+    bool extractStudentInfo(const QString& response);
+    bool extractStudentAvgs(const QString& response);
 public:
+    enum user_info {
+        Number,
+        Name,
+        Field,
+        StudyType,
+        TotalAvg,
+        Passed
+    };
+    Q_ENUM(user_info);
+
     DashboardHandler();
+
+private slots:
+    void parseUserInfo(QNetworkReply& reply);
+    void parseUserNumber(QNetworkReply& reply);
+    void parseTokens(QNetworkReply& reply);
+
+public slots:
+    void start();
 };
 
 #endif // DASHBOARDHANDLER_H
--- source/dashboardhandler.cpp
+++ source/dashboardhandler.cpp
@@ -1,6 +1,162 @@
-#include "dashboardhandler.h"
+#include "header/dashboardhandler.h"
 
-DashboardHandler::DashboardHandler()
+DashboardHandler::DashboardHandler() : student_info {"", "", "", "", "", ""}
 {
 
+}
+
+void DashboardHandler::start()
+{
+    getTokens();
+}
+
+bool DashboardHandler::getTokens()
+{
+    connect(&request, &Network::complete, this, &DashboardHandler::parseTokens);
+    request.setUrl(root_url + user_info_url + request_validators["tck"]);
+    qDebug() << getCookies().toUtf8();
+    request.addHeader("Cookie", getCookies().toUtf8());
+    return request.get();
+}
+
+void DashboardHandler::parseTokens(QNetworkReply& reply)
+{
+    disconnect(&request, &Network::complete, this, &DashboardHandler::parseTokens);
+    QString data;
+    if (!verifyResponse(reply, data)) return;
+
+    request_validators.insert(TextParser::Validators::extractFormValidators(data));
+
+    getUserNumber();
+}
+
+bool DashboardHandler::getUserNumber()
+{
+    connect(&request, &Network::complete, this, &DashboardHandler::parseUserNumber);
+    request.setUrl(root_url + user_info_url + request_validators["tck"]);
+    request.addHeader("Content-Type", "application/x-www-form-urlencoded");
+    request.addHeader("Cookie", getCookies().toUtf8());
+
+    QString data{"__VIEWSTATE="             + QUrl::toPercentEncoding(request_validators["__VIEWSTATE"])
+                + "&__VIEWSTATEGENERATOR="  + request_validators["__VIEWSTATEGENERATOR"]
+                + "&__EVENTVALIDATION="     + QUrl::toPercentEncoding(request_validators["__EVENTVALIDATION"])
+                + "&TicketTextBox="         + request_validators["tck"]
+                + "Fm_Action=00&Frm_Type=&Frm_No=&XMLStdHlp=&TxtMiddle=<r/>&ex="};
+
+    return request.post(data.toUtf8());
+}
+
+void DashboardHandler::parseUserNumber(QNetworkReply& reply)
+{
+    disconnect(&request, &Network::complete, this, &DashboardHandler::parseUserNumber);
+
+    QString data, user_number;
+    if (!verifyResponse(reply, data)) return;
+
+    request_validators.insert(TextParser::Validators::extractFormValidators(data));
+    user_number = TextParser::extractStudentNumber(data);
+    if (user_number == QString()) {
+        setErrorCode(Constants::Errors::UnknownError);
+        reply.deleteLater();
+        setSuccess(false);
+        setFinished(true);
+        return;
+    }
+    student_info[user_info::Number] = user_number;
+    getUserInfo();
+}
+
+bool DashboardHandler::getUserInfo()
+{
+    connect(&request, &Network::complete, this, &DashboardHandler::parseUserInfo);
+    request.setUrl(root_url + user_info_url + request_validators["tck"]);
+    request.addHeader("Content-Type", "application/x-www-form-urlencoded");
+    request.addHeader("Cookie", getCookies().toUtf8());
+
+    QString data{"__VIEWSTATE="                  + QUrl::toPercentEncoding(request_validators["__VIEWSTATE"])
+                + "&__VIEWSTATEGENERATOR="       + request_validators["__VIEWSTATEGENERATOR"]
+                + "&__EVENTVALIDATION="          + QUrl::toPercentEncoding(request_validators["__EVENTVALIDATION"])
+                + "&TicketTextBox="              + request_validators["tck"]
+                + "&TxtMiddle=%3Cr+F41251%3D%22" + student_info[Number]
+                + "%22%2F%3EFm_Action=00&Frm_Type=&Frm_No=&XMLStdHlp=&ex="};
+
+    return request.post(data.toUtf8());
+}
+
+void DashboardHandler::parseUserInfo(QNetworkReply& reply)
+{
+    disconnect(&request, &Network::complete, this, &DashboardHandler::parseUserInfo);
+    QString data;
+    if (!verifyResponse(reply, data)) return;
+
+    if (!extractStudentInfo(data) || !extractStudentAvgs(data)) {
+        setErrorCode(Constants::Errors::ExtractError);
+        setSuccess(false);
+        setFinished(true);
+        return;
+    }
+    qDebug() << student_info;
+    qDebug() << student_avgs;
+    setSuccess(true);
+    setFinished(true);
+    return;
+}
+
+bool DashboardHandler::extractStudentInfo(const QString& response)
+{
+    const QList<QString> keywords {"F51851",        // keyword for Name
+                                   "F17551",        // Field
+                                   "F41351",        // StudyType
+                                   "F41701",        // TotalAvg
+                                   "F41801"};      // Passed
+    int position;
+    QString value;
+    for (QString keyword : keywords) {
+        position = response.indexOf(keyword);
+        if (position == -1) {
+            return false;
+        }
+        // 10 is the lentgh of actual keyword which for example is "F51851 = '"
+        // so we need to skip this
+        position += 10;
+        for (int i{position}; response[i] != "'"; ++i) {
+            value.append(response[i]);
+        }
+        student_info.append(value);
+        value.clear();
+    }
+    return true;
+}
+
+bool DashboardHandler::extractStudentAvgs(const QString &response)
+{
+    int year_position, avg_position;
+    QString year_value, avg_value;
+    const QString year_keyword {"F4350"}, avg_keyword {"CumGPA"};
+    year_position = response.indexOf(year_keyword);
+    avg_position = response.indexOf(avg_keyword);
+    if (year_position == -1 || avg_position == -1) {
+        return false;
+    }
+
+    while (year_position != -1 && avg_position != -1) {
+        // 7  and 8 are the lentgh of actual keywords which for example is 'F51851="'
+        // so we need to skip this
+        year_position += 7;
+        avg_position += 8;
+        for (int i{year_position}; response[i] != '"'; ++i) {
+            year_value.append(response[i]);
+        }
+
+        for (int i{avg_position}; response[i] != ' '; ++i) {
+            avg_value.append(response[i]);
+        }
+
+        student_avgs.append(QPair<int, float>(year_value.toInt(), avg_value.toFloat()));
+        year_value.clear();
+        avg_value.clear();
+        year_position = response.indexOf(year_keyword, year_position);
+        avg_position = response.indexOf(avg_keyword, avg_position);
+    }
+    return true;
 }
