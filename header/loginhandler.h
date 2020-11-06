f23545e9caf2e9346df696aa91f8881674211d33
SeedPuller <SeedPuller@gmail.com>
2020-11-06 18:52:55 +0330

    added a name extractor function and user name property for qml

--- header/loginhandler.h
+++ header/loginhandler.h
@@ -7,14 +7,18 @@ class LoginHandler : public Handler
 {
     Q_OBJECT
 private:
-    const QString login_url{"/Forms/AuthenticateUser/AuthUser.aspx?fid=0;1&tck=&&&lastm=20190219160242"};
+    const QString   login_url{"/Forms/AuthenticateUser/AuthUser.aspx?fid=0;1&tck=&&&lastm=20190219160242"};
+    QString         user_name;
+
+    bool            extractName (QString& response);
 
 private slots:
-    bool parseLogin(QNetworkReply& reply);
+    bool            parseLogin  (QNetworkReply& reply);
 
 public:
     LoginHandler();
-    Q_INVOKABLE bool tryLogin(const QString username, const QString password, const QString captcha);
+    Q_INVOKABLE bool    tryLogin(const QString username, const QString password, const QString captcha);
+    Q_INVOKABLE QString getName() const;
 };
 
 #endif // LOGINHANDLER_H
--- source/loginhandler.cpp
+++ source/loginhandler.cpp
@@ -24,8 +24,40 @@ bool LoginHandler::parseLogin(QNetworkReply& reply)
     disconnect(&request, &Network::complete, this, &LoginHandler::parseLogin);
     QString data;
     if (!verifyResponse(reply, data)) return false;
+    if (!extractName(data)) {
+        setErrorCode(Constants::Errors::ExtractError);
+        reply.deleteLater();
+        setSuccess(false);
+        setFinished(true);
+        return false;
+    }
     reply.deleteLater();
     setSuccess(true);
     setFinished(true);
     return true;
+}
+
+
+bool LoginHandler::extractName(QString& response)
+{
+    const QString keyword {"SetUsr('"};
+    int position {response.indexOf(keyword)};
+    if (position == -1) return false;
+    position += 8;
+    QChar character;
+    user_name.clear();
+    while (response[position] != ")") {
+        character = response[position];
+        if (character != '\'') {
+            user_name.append(character);
+        }
+        ++position;
+    }
+    user_name.replace(',', ' ');
+    return true;
+}
+
+QString LoginHandler::getName() const
+{
+    return user_name;
 }
