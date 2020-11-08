88b56fc499b01da1bb5d4781b12d21d2da3a5423
SeedPuller <SeedPuller@gmail.com>
2020-11-08 23:54:30 +0330

    comments for LoginHandler class added

--- header/loginhandler.h
+++ header/loginhandler.h
@@ -1,6 +1,12 @@
 #ifndef LOGINHANDLER_H
 #define LOGINHANDLER_H
 
+/*
+ * Class: LoginHandler
+ * Files: loginhandler.h, loginhandler.cpp
+ * This class handle login things and extract student name after successful login.
+ */
+
 #include "handler.h"
 
 class LoginHandler : public Handler
@@ -10,14 +16,18 @@ private:
     const QString   login_url{"/Forms/AuthenticateUser/AuthUser.aspx?fid=0;1&tck=&&&lastm=20190219160242"};
     QString         user_name;
 
+    // extract student name and store it into 'user_name' by parsing 'response'
     bool            extractName (QString& response);
 
 private slots:
+    // return true if login was successful by parsing 'reply'
     bool            parseLogin  (QNetworkReply& reply);
 
 public:
     LoginHandler();
+    // send a request for login
     Q_INVOKABLE bool    tryLogin(const QString username, const QString password, const QString captcha);
+    // return 'user_name'
     Q_INVOKABLE QString getName() const;
 };
 
--- source/loginhandler.cpp
+++ source/loginhandler.cpp
@@ -5,10 +5,15 @@ LoginHandler::LoginHandler()
 
 }
 
+/*
+ * prepare a request and send it to log in to Golestan system
+ */
 bool LoginHandler::tryLogin(const QString username, const QString password, const QString captcha)
 {
     connect(&request, &Network::complete, this, &LoginHandler::parseLogin);
+    // credentials would bind here
     QString logincreds = QString("<r F51851=\"\" F80351=\"%1\" F80401=\"%2\" F51701=\"%3\" F83181=\"\"/>").arg(username, password, captcha);
+    // data values should be in url-encoded format
     QString data{"__VIEWSTATE=" + QUrl::toPercentEncoding(request_validators["__VIEWSTATE"])
                 + "&__VIEWSTATEGENERATOR=" + request_validators["__VIEWSTATEGENERATOR"]
                 + "&__EVENTVALIDATION=" + QUrl::toPercentEncoding(request_validators["__EVENTVALIDATION"])
@@ -24,6 +29,7 @@ bool LoginHandler::parseLogin(QNetworkReply& reply)
     disconnect(&request, &Network::complete, this, &LoginHandler::parseLogin);
     QString data;
     if (!verifyResponse(reply, data)) return false;
+    // extract student name
     if (!extractName(data)) {
         setErrorCode(Constants::Errors::ExtractError);
         reply.deleteLater();
@@ -43,9 +49,12 @@ bool LoginHandler::extractName(QString& response)
     const QString keyword {"SetUsr('"};
     int position {response.indexOf(keyword)};
     if (position == -1) return false;
+    // 8 is the length of "SetUsr('". we should skip this to capture the main value
     position += 8;
     QChar character;
     user_name.clear();
+    // name in the response is like this: 'first name', 'lastname')
+    // we skip the single-qoutes and at last remove the comma.
     while (response[position] != ")") {
         character = response[position];
         if (character != '\'') {
