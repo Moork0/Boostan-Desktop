35a62602a27eae3975ad6b8e4af8ef842669ffa2
SeedPuller <SeedPuller@gmail.com>
2020-11-12 00:52:18 +0330

    used QStringLiteral for string literals

--- header/helpers/constants.h
+++ header/helpers/constants.h
@@ -13,13 +13,13 @@ using QHashString = QHash <QString, QString>;
 namespace Constants {
 
 
-    inline const QString    application_name{"Boostan"},
-                            organization_name{"AVID"},
-                            domain_name{"SeedPuller.space"},
+    inline const QString    application_name{QStringLiteral("Boostan")},
+                            organization_name{QStringLiteral("AVID")},
+                            domain_name{QStringLiteral("SeedPuller.space")},
                             application_path {QDir::currentPath() + "/"},
                             // default golestan url for inserting to settings if no configuration is available
-                            root_url {"https://golestan.umz.ac.ir"},
-                            settings_path {application_path + "settings.ini"};
+                            root_url {QStringLiteral("https://golestan.umz.ac.ir")},
+                            settings_path {application_path + QStringLiteral("settings.ini")};
 
     /**
         * TODO: Constants for errors.
@@ -77,37 +77,37 @@ namespace Constants {
         */
         inline const QHash<int, QString> error_keywords
         {
-            {WrongCaptcha, "كد امنيتي"}     // error strings that has this keyword are about WrongCaptcha
+            {WrongCaptcha, QStringLiteral("كد امنيتي")}     // error strings that has this keyword are about WrongCaptcha
         };
 
         // Error title for each error code that identifyed so far
         inline const QHash<uint, QString> error_strings
         {
-            {ServerConnenctionError, "خطا در اتصال به سرور"},
-            {UnknownError, "اوه! یک خطای ناشناخته رخ داده!"},
-            {WrongCaptcha, "کد امنیتی اشتباه وارد شده!"},
-            {CaptchaStoreError, "مشکلی در ذخیره تصویر امنیتی بوجود اومده!"},
-            {SettingsError, "مشکلی در تنظیمات بوستان بوجود اومده!"},
-            {ExtractError, "مشکلی در استخراج اطلاعات از گلستان بوجود اومده!"},
+            {ServerConnenctionError, QStringLiteral("خطا در اتصال به سرور")},
+            {UnknownError, QStringLiteral("اوه! یک خطای ناشناخته رخ داده!")},
+            {WrongCaptcha, QStringLiteral("کد امنیتی اشتباه وارد شده!")},
+            {CaptchaStoreError, QStringLiteral("مشکلی در ذخیره تصویر امنیتی بوجود اومده!")},
+            {SettingsError, QStringLiteral("مشکلی در تنظیمات بوستان بوجود اومده!")},
+            {ExtractError, QStringLiteral("مشکلی در استخراج اطلاعات از گلستان بوجود اومده!")},
             // this is built-in Golestan error codes that we might see.
-            {1, "نام کاربری یا رمز عبوری که وارد شده اشتباهه!"},
-            {24, "گلستان میگه دوباره باید وارد بشی!"},
-            {27, "تعداد تلاش ناموفق برای ورود بیش از حد مجاز شده!"}
+            {1, QStringLiteral("نام کاربری یا رمز عبوری که وارد شده اشتباهه!")},
+            {24, QStringLiteral("گلستان میگه دوباره باید وارد بشی!")},
+            {27, QStringLiteral("تعداد تلاش ناموفق برای ورود بیش از حد مجاز شده!")}
         };
 
         // Error description for each error code that identifyed so far
         inline const QHash<uint, QString> error_solutions
         {
-            {ServerConnenctionError, "لطفا وضعیت اتصال به اینترنت و وبسایت گلستان رو بررسی کنید"},
-            {UnknownError, "از اونجایی که منم نمیدونم چه خطاییه، بهتره لاگ هارو بررسی کنی و در صفحه گیتهاب این مشکل رو گزارش بدی"},
-            {WrongCaptcha, "دوباره با دقت تلاش کن :)"},
-            {CaptchaStoreError, "دسترسی های پوشه بوستان رو بررسی کنید و برنامه رو دوباره اجرا کنید"},
-            {SettingsError, "دسترسی های فایل تنظیمات که در پوشه برنامه قرار داره رو بررسی کنید"},
-            {ExtractError, "یکبار دیگه تلاش کن تا بوستان این مشکل رو برات حل کنه"},
+            {ServerConnenctionError, QStringLiteral("لطفا وضعیت اتصال به اینترنت و وبسایت گلستان رو بررسی کنید")},
+            {UnknownError, QStringLiteral("از اونجایی که منم نمیدونم چه خطاییه، بهتره لاگ هارو بررسی کنی و در صفحه گیتهاب این مشکل رو گزارش بدی")},
+            {WrongCaptcha, QStringLiteral("دوباره با دقت تلاش کن :)")},
+            {CaptchaStoreError, QStringLiteral("دسترسی های پوشه بوستان رو بررسی کنید و برنامه رو دوباره اجرا کنید")},
+            {SettingsError, QStringLiteral("دسترسی های فایل تنظیمات که در پوشه برنامه قرار داره رو بررسی کنید")},
+            {ExtractError, QStringLiteral("یکبار دیگه تلاش کن تا بوستان این مشکل رو برات حل کنه")},
             // this is built-in Golestan error codes that we might see.
-            {1, "دوباره با دقت تلاش کن :)"},
-            {24, "دوباره برای ورود تلاش کن تا این مشکل رفع بشه. اگر نشد لطفا در گیتهاب خبر بده"},
-            {27, "یکبار دیگه تلاش کن تا بوستان این مشکل رو برات حل کنه"}
+            {1, QStringLiteral("دوباره با دقت تلاش کن :)")},
+            {24, QStringLiteral("دوباره برای ورود تلاش کن تا این مشکل رفع بشه. اگر نشد لطفا در گیتهاب خبر بده")},
+            {27, QStringLiteral("یکبار دیگه تلاش کن تا بوستان این مشکل رو برات حل کنه")}
         };
     };
 };
