b4060641e56dd7dde3f57981a80acebe669f498f
SeedPuller <SeedPuller@gmail.com>
2020-11-12 18:19:28 +0330

    used QStringLiteral for string literals

--- header/handlers/loginhandler.h
+++ header/handlers/loginhandler.h
@@ -13,7 +13,7 @@ class LoginHandler : public Handler
 {
     Q_OBJECT
 private:
-    const QString   login_url{"/Forms/AuthenticateUser/AuthUser.aspx?fid=0;1&tck=&&&lastm=20190219160242"};
+    const QString   login_url{QStringLiteral("/Forms/AuthenticateUser/AuthUser.aspx?fid=0;1&tck=&&&lastm=20190219160242")};
     QString         user_name;
 
     // extract student name and store it into 'user_name' by parsing 'response'
