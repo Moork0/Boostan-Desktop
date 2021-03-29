324d7755934b6f9ae88bf3380f93fc51bd46ecde
SeedPuller <SeedPuller@gmail.com>
2021-03-29 21:02:18 +0430

    Added comments for abstractxmldatahandler.h

--- header/handlers/abstractxmldatahandler.h
+++ header/handlers/abstractxmldatahandler.h
@@ -1,6 +1,15 @@
 #ifndef ABSTRACTXMLDATAHANDLER_H
 #define ABSTRACTXMLDATAHANDLER_H
 
+/*
+    * Class: AbstractXmlDataHandler
+    * Files: abstractxmldatahandler.h and abstractxmldatahandler.cpp
+    * This is a ABSTRACT class for being drived by classes that tend to parse XML data
+    * which recieved from Golestan system.
+
+    * Note: You MUST implement 'getIsEmpty' which determines the empty-ness of a DS.
+*/
+
 #include "handler.h"
 #include <QXmlStreamReader>
 
@@ -13,14 +22,16 @@ private:
     Q_PROPERTY(bool is_empty READ getIsEmpty NOTIFY isEmptyChanged)
 
 protected:
+    // REGEX pattern for extracting the XML data withing another data's
     const QString   xmldata_pattern       {QStringLiteral("<Root>[\\W\\w]+<\\/Root>")};
     bool is_empty;
 
 public:
     AbstractXmlDataHandler();
-    void setIsEmpty(bool state);
+    // set state to is_empty
+    void            setIsEmpty(bool state);
 
-    virtual bool getIsEmpty() const = 0;
+    virtual bool    getIsEmpty() const = 0;
 
 signals:
     void isEmptyChanged();
