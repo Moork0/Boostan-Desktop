ab4ae4f1f592280edc63559e2173fa5b97a504ed
SeedPuller <SeedPuller@gmail.com>
2021-04-05 16:30:48 +0430

    Created new handler

---
+++ header/handlers/scoreshandler.h
@@ -0,0 +1,14 @@
+#ifndef SCORESHANDLER_H
+#define SCORESHANDLER_H
+
+#include "abstractxmldatahandler.h"
+
+class ScoresHandler : public AbstractXmlDataHandler
+{
+    Q_OBJECT
+
+public:
+    ScoresHandler();
+};
+
+#endif // SCORESHANDLER_H
---
+++ source/handlers/scoreshandler.cpp
@@ -0,0 +1,6 @@
+#include "header/handlers/scoreshandler.h"
+
+ScoresHandler::ScoresHandler()
+{
+
+}
