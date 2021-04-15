fe91d30ea76b4981a6c12c41f2952123bcca44ce
SeedPuller <SeedPuller@gmail.com>
2021-04-15 13:07:16 +0430

    Implemeted requests to Golestan in ScoresHandler class

--- header/handlers/scoreshandler.h
+++ header/handlers/scoreshandler.h
@@ -11,15 +11,31 @@ class ScoresHandler : public AbstractXmlDataHandler
 private:
     QVariantList _scores;
     QVariantMap _score_brief;
+    // the semester number we wanna get information of
+    int _semester;
+    QString _student_id;
+
+    // url of section we gonna send our requests.
+    const inline static QString       _scores_url {QStringLiteral("/Forms/F1802_PROCESS_MNG_STDJAMEHMON/F1802_01_PROCESS_MNG_STDJAMEHMON_Dat.aspx?r=0.9638806400489983&fid=0;12310&b=0&l=0&&lastm=20180201081222&tck=")};
+
 
     // Forced implementation of AbstractXmlDataHandler pure function
     bool    getIsEmpty              () const override;
+
+    //
+    void    requestTokens();
+    void    requestScores();
 
     // normalize the name and remove ugly chars
     void    normalizeName(QString& name);
     bool    extractScores(const QString& data);
     bool    extractBirefScores(const QString& data);
     QString extractXmlAttr(const QString& data, const QString& key, const bool search_at_start = true) const;
+
+private slots:
+    // parse the response to requestTokens()
+    void            parseTokens(QNetworkReply& reply);
+    void            parseScores(QNetworkReply& reply);
 
 public:
     enum status
@@ -35,7 +51,8 @@ public:
     ScoresHandler();
 
 public slots:
-    void start();
+    void start(const int semester, const QString student_id);
+    void getScoresOf(const int semester);
     QVariantList getScores() const;
     QVariantList getBriefScores() const;
 };
--- source/handlers/scoreshandler.cpp
+++ source/handlers/scoreshandler.cpp
@@ -5,6 +5,91 @@ bool ScoresHandler::getIsEmpty() const
     return is_empty;
 }
 
+void ScoresHandler::start(const int semester, const QString student_id)
+{
+    _semester = semester;
+    _student_id = student_id;
+    requestTokens();
+}
+
+void ScoresHandler::getScoresOf(const int semester)
+{
+    _semester = semester;
+    requestScores();
+
+}
+
+void ScoresHandler::requestTokens()
+{
+    connect(&request, &Network::complete, this, &ScoresHandler::parseTokens);
+    QString tck_token {getTckToken()};
+    request.setUrl(root_url + _scores_url + tck_token);
+    request.addHeader("Cookie", getCookies().toUtf8());
+    request.get();
+}
+
+void ScoresHandler::parseTokens(QNetworkReply &reply)
+{
+    disconnect(&request, &Network::complete, this, &ScoresHandler::parseTokens);
+    QString data;
+    if (!verifyResponse(reply, data)) {
+        reply.deleteLater();
+        setSuccess(false);
+        setFinished(true);
+    }
+
+    reply.deleteLater();
+    request_validators.insert(extractFormValidators(data));
+    requestScores();
+}
+
+void ScoresHandler::requestScores()
+{
+    connect(&request, &Network::complete, this, &ScoresHandler::parseScores);
+    _scores.clear();
+
+    request.setUrl(root_url + _scores_url + request_validators["tck"]);
+    request.addHeader("Content-Type", "application/x-www-form-urlencoded");
+    request.addHeader("Cookie", getCookies().toUtf8());
+
+    QString ticket_tbox {getTckToken()};
+    QString param_txtmiddle {QString(QStringLiteral("<r F41251=\"%1\" F43501=\"%2\"/>")).arg(_student_id, QString::number(_semester))};
+    QString data{
+            QStringLiteral("__VIEWSTATE=")             % QUrl::toPercentEncoding(request_validators["__VIEWSTATE"])
+            % QStringLiteral("&__VIEWSTATEGENERATOR=")  % request_validators["__VIEWSTATEGENERATOR"]
+            % QStringLiteral("&__EVENTVALIDATION=")     % QUrl::toPercentEncoding(request_validators["__EVENTVALIDATION"])
+            % QStringLiteral("&TicketTextBox=")         % ticket_tbox
+            % QStringLiteral("&TxtMiddle=")             % QUrl::toPercentEncoding(param_txtmiddle)
+            % QStringLiteral("&Fm_Action=80&Frm_Type=&Frm_No=&XMLStdHlp=&ex=")
+                };
+
+    request.post(data.toUtf8());
+}
+
+void ScoresHandler::parseScores(QNetworkReply &reply)
+{
+    disconnect(&request, &Network::complete, this, &ScoresHandler::parseScores);
+    bool parse_success {true};
+
+    QString data;
+    if (!verifyResponse(reply, data))
+        parse_success = false;
+
+    if (!extractScores(data) || !extractBirefScores(data)) {
+        setErrorCode(Errors::ExtractError);
+        parse_success = false;
+    }
+
+    reply.deleteLater();
+    if (!parse_success) {
+        setSuccess(false);
+        setFinished(true);
+        return;
+    }
+    setSuccess(true);
+    setFinished(true);
+}
+
 void ScoresHandler::normalizeName(QString &name)
 {
     name.replace(QStringLiteral("ك"), QStringLiteral("ک"));
@@ -111,22 +196,6 @@ QString ScoresHandler::extractXmlAttr(const QString &data, const QString& key, c
 ScoresHandler::ScoresHandler()
 {
 
-}
-
-void ScoresHandler::start()
-{
-    _scores.clear();
-    QDir::setCurrent("/home/moorko/cpp/boostan/boostan/test/");
-    QFile file("res2.html");
-    if (file.open(QIODevice::ReadOnly)) {
-        QString rr {file.readAll()};
-        extractScores(rr);
-        extractBirefScores(rr);
-    } else {
-        qDebug() << file.errorString();
-    }
-    setSuccess(true);
-    setFinished(true);
 }
 
 QVariantList ScoresHandler::getScores() const
