d41d2424056c05ebdb0edb10acea78d1383f57b2
SeedPuller <SeedPuller@gmail.com>
2021-05-29 13:18:47 +0430

    Added a toda_date constant and fixed the storage specifiers in constants.h

--- header/helpers/constants.h
+++ header/helpers/constants.h
@@ -7,19 +7,33 @@
 */
 
 #include <QDir>
+#include <QCalendar>
+#include <QDate>
 
 using QHashString = QHash <QString, QString>;
-
 namespace Constants {
 
+    static inline QString generateTodayDate ()
+    {
+        QLocale locale {QLocale::Persian, QLocale::Iran};
+        locale.setNumberOptions(QLocale::OmitGroupSeparator);
 
-    inline const QString    application_name{QStringLiteral("Boostan")},
-                            organization_name{QStringLiteral("AVID")},
-                            domain_name{QStringLiteral("SeedPuller.ir")},
-                            application_path {QDir::currentPath() + "/"},
-                            // default golestan url for inserting to settings if no configuration is available
-                            root_url {QStringLiteral("https://golestan.umz.ac.ir")},
-                            settings_path {application_path + QStringLiteral("settings.ini")};
+        QCalendar calendar(QCalendar::System::Jalali);
+        QDate today = QDate::currentDate();
+        QCalendar::YearMonthDay ymd = calendar.partsFromDate(today);
+
+        return QString(QStringLiteral("%1 %2 %3"))
+                .arg(locale.toString(ymd.day), calendar.monthName(locale, ymd.month), locale.toString(ymd.year));
+    }
+
+    static inline const QString application_name{QStringLiteral("Boostan")},
+                                organization_name{QStringLiteral("AVID")},
+                                domain_name{QStringLiteral("SeedPuller.ir")},
+                                application_path {QDir::currentPath() + "/"},
+                                // default golestan url for inserting to settings if no configuration is available
+                                root_url {QStringLiteral("https://golestan.umz.ac.ir")},
+                                settings_path {application_path + QStringLiteral("settings.ini")},
+                                today_date {generateTodayDate()};
 
 };  // namespace Constants
 
