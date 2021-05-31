13013c1069b0a09bf8ebfa7a610a5825e3fc94b2
SeedPuller <SeedPuller@gmail.com>
2021-05-31 22:56:39 +0430

    Added comments and MacOS support

--- header/helpers/commonmacros.h
+++ header/helpers/commonmacros.h
@@ -1,13 +1,22 @@
 #ifndef COMMONMACROS_H
 #define COMMONMACROS_H
 
-#include <qglobal.h>
+/*
+    * file name: commonmacros.h
+    * This file suppose to store all of the Macro's we might use in our code.
+*/
+
+#include <QtGlobal>
 #include <QString>
 
+// Just use QStringLiteral when we are not in windows
 #if defined (Q_OS_WINDOWS)
     #define MyStringLiteral(str) \
             str
 #elif defined (Q_OS_LINUX)
+    #define MyStringLiteral(str) \
+            QStringLiteral(str)
+#elif defined (Q_OS_MACOS)
     #define MyStringLiteral(str) \
             QStringLiteral(str)
 #endif
